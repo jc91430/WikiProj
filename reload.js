ad8f2a868a0b563aee2d210cd29ee447f43c1395
Jeff Chen <jeffchen91430@gmail.com>
2015-12-28 18:51:50 -0500

    Test cookies

--- reload.js
+++ reload.js
@@ -1,15 +1,25 @@
 //Reload the iframe with a random page from one interested category
 //Need to add in mobile/desktop switching. Currently locked into mobile
-function reload() {
-  var pages = getCategories();
-  var n = Math.floor(Math.random() * pages.length);
+var hardcode = ["Beaches_of_Japan", "Pastries", "Tea", "Island_countries", "Gundam"];
 
-  console.log("Current category: " + pages[n]);
+function reload() {
+
+  setPreferences();
+
+  var categories = getCategories();
+  var n = Math.floor(Math.random() * categories.length);
+
+  console.log("Current category: " + categories[n]);
   document.getElementById("frame").src =
-    "https://en.m.wikipedia.org/wiki/Special:RandomInCategory/" + pages[n];
+    "https://en.m.wikipedia.org/wiki/Special:RandomInCategory/" + categories[n];
 }
 
 //Return array of user-interested categories
 function getCategories() {
-    return ["Beaches_of_Japan", "Pastries", "Tea", "Island_countries", "Gundam"];
+  return document.cookie.split("#");
+}
+
+function setPreferences() {
+  document.cookie = "preferences=" + hardcode.join("#") + ";";
+  console.log(document.cookie + " set as cookie");
 }
